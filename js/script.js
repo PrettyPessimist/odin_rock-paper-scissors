afd9b743aeb4bf6ef77720b7b3f818370003c75f
Luke <182971829+PrettyPessimist@users.noreply.github.com>
2024-12-03 10:29:06 -0600

    finish user choice function
    Finished function for user's choice between 'rock', 'paper', and
    'scissors.' Function is also case insensitive.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,19 @@ function getCPUChoice() {
     return cpuChoice;
 }
 
-
+function getUserChoice(){
+    let userAnsw = prompt("Rock, Paper, or Scissors?");
+    userAnsw = userAnsw.toLowerCase();
+    let userChoice = (userAnsw === "rock") ? 1 :
+                     (userAnsw === "paper") ? 2 :
+                     (userAnsw === "scissors") ? 3:
+                     0;
+    if(userChoice === 0){
+        alert("You have wasted a turn, good job!");
+        return userChoice;
+    }
+    return userChoice;
+}
 
 
 
