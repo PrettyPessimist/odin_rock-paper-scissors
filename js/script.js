5410ae01f87103a37f1e8654a305067a16499afd
Luke <182971829+PrettyPessimist@users.noreply.github.com>
2024-12-03 11:08:55 -0600

    finish 'gameplay' loop/playRound function
    Finished logic for playing a round of rock, paper, scissors. Also added
    global variables for user and computer scores.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-
+let cpuScore = 0;
+let userScore = 0;
 
 function getCPUChoice() {
     let cpuChoice = Math.floor(Math.random()*3)+1;
@@ -17,6 +18,36 @@ function getUserChoice(){
         return userChoice;
     }
     return userChoice;
+}
+
+function playRound(){
+    cpuChoice = getCPUChoice();
+    userChoice = getUserChoice();
+    let usrChoiceWord = (userChoice === 1) ? "Rock":
+                        (userChoice === 2) ? "Paper":
+                        (userChoice === 3) ? "Scissors":
+                        "error";
+
+    let cpuChoiceWord = (cpuChoice === 1) ? "Rock":
+                        (cpuChoice === 2) ? "Paper":
+                        (cpuChoice === 3) ? "Scissors":
+                        "error";
+
+    let result = 
+    (userChoice === cpuChoice) ? "tie" :
+    ((userChoice === 1 && cpuChoice === 3) || (userChoice === 2 && cpuChoice === 1) || (userChoice === 3 && cpuChoice === 2)) ? "user win":
+    ((userChoice === 3 && cpuChoice === 1) || (userChoice === 1 && cpuChoice === 2) || (userChoice === 2 && cpuChoice === 3)) ? "computer win":
+    "error";
+
+    if(result ==="user win"){
+        userScore = userScore + 1;
+        return alert("You won! " + usrChoiceWord + " beats "+cpuChoiceWord);
+    } else if(result === "computer win"){
+        cpuScore = cpuScore + 1;
+        return alert("You lost! " + cpuChoiceWord + " beats "+usrChoiceWord);
+    } else if (result === "tie"){
+        playRound();
+    }
 }
 
 
