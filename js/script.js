f6e64d4318fe8512985bd28a4951a17bf8a28e2a
Luke <182971829+PrettyPessimist@users.noreply.github.com>
2024-12-03 14:16:21 -0600

    improve playRound() function/gameplay loop
    improved playRound() func and added option for user mistype error.

--- js/script.js
+++ js/script.js
@@ -37,16 +37,24 @@ function playRound(){
     (userChoice === cpuChoice) ? "tie" :
     ((userChoice === 1 && cpuChoice === 3) || (userChoice === 2 && cpuChoice === 1) || (userChoice === 3 && cpuChoice === 2)) ? "user win":
     ((userChoice === 3 && cpuChoice === 1) || (userChoice === 1 && cpuChoice === 2) || (userChoice === 2 && cpuChoice === 3)) ? "computer win":
+    (userChoice === 0) ? "user error":
     "error";
 
     if(result ==="user win"){
         userScore = userScore + 1;
+        console.log(userScore);
         return alert("You won! " + usrChoiceWord + " beats "+cpuChoiceWord);
     } else if(result === "computer win"){
         cpuScore = cpuScore + 1;
+        console.log(cpuScore);
         return alert("You lost! " + cpuChoiceWord + " beats "+usrChoiceWord);
     } else if (result === "tie"){
+        alert("You tied? Play again!");
         playRound();
+    } else if (result == "user error"){
+        cpuScore = cpuScore + 1;
+        console.log(cpuScore);
+        return alert("You lost! " + cpuChoiceWord + " beats your gibberish");
     }
 }
 
